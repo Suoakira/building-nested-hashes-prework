18bab78f839e31b551883d3029b690f258e8f6f8
Stephen Courtier <steves0089@gmail.com>
2018-09-11 12:55:44 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,11 +14,7 @@ def third_challenge
         :age => "15",
         :status => "alive"
       },
-      :hero_friends => [{
-        :name => "Steven",
-        :age => "30",
-        :attitude => "confused"
-      }
+      :hero_friends => [
         ]
    }, 
    :capulet => {
