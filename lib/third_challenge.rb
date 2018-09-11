09b38b1619624f1fe3084b85bb3a3278793c581c
Stephen Courtier <steves0089@gmail.com>
2018-09-11 12:58:02 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,7 +14,16 @@ def third_challenge
         :age => "15",
         :status => "alive"
       },
-      :hero_friends => [
+      :hero_friends => [{
+        :name => "Benvolio",
+        :age => "17",
+        :attitude => "worried"
+      },
+      {
+        :name => "Mercutio",
+        :age => "18",
+        :attitude => "hot-headed"
+      }
         ]
    }, 
    :capulet => {
@@ -32,6 +41,11 @@ def third_challenge
         :status => "alive"
       },
       :heroine_friends => [{
+        :name => "Steven",
+        :age => "30",
+        :attitude => "confused"
+      },
+      {
         :name => "Nurse",
         :age => "44",
         :attitude => "worried"
